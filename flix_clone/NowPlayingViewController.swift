becbef8b262bef87cbe5548bc5852a91be4d8cf5
Guanxin <liguanxin078@gmail.com>
2018-02-01 07:29:33 -0800

    link data, tableView

---
+++ flix_clone/NowPlayingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NowPlayingViewController.swift
+//  flix_clone
+//
+//  Created by Guanxin Li on 2/1/18.
+//  Copyright © 2018 Guanxin. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        let url = URL(string:
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy:
+            .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil,
+                                 delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will runs when network returns
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDicationary = try! JSONSerialization
+                .jsonObject(with: data, options:[]) as! [String: Any]
+                let movies = dataDicationary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume();
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
