a64ac5b2104d0c08acc06fffc257722bd665f137
Guanxin <liguanxin078@gmail.com>
2018-02-12 20:42:39 -0800

    superhero detailed view

---
+++ flix_clone/ViewControllers/SuperheroDetailedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SuperheroDetailedViewController.swift
+//  flix_clone
+//
+//  Created by Guanxin Li on 2/12/18.
+//  Copyright © 2018 Guanxin. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroDetailedViewController: UIViewController {
+
+    
+    @IBOutlet weak var backDropImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
